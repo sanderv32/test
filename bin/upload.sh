0009cdef1f6cee5de9823fa433da233d5fe5b62d
Alexander Verhaar <sanderv32@gmail.com>
2017-12-31 01:49:26 +0100

    [skip ci] Only update json and tar.gz files

--- bin/upload.sh
+++ bin/upload.sh
@@ -9,7 +9,7 @@ git config --global user.name "Travis build bot"
 
 git remote rm origin
 git remote add origin https://sanderv32:$GITHUB_API_KEY@github.com/sanderv32/test.git
-git add -f .
+git add -f *.tar.gz *.json
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
 git push -fq origin master > /dev/null
 echo -e "Done\n"
